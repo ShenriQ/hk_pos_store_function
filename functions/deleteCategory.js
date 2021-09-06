edb64f1b4e793d4d51ffcafbcd0840971b5fdb28
dev <dev@dev.com>
2021-09-06 02:16:23 +0200

    delete category, delete service category  =>  Cors error fix

--- functions/deleteCategory.js
+++ functions/deleteCategory.js
@@ -1,38 +1,41 @@
 const admin = require('firebase-admin');
 const db = admin.firestore();
+const cors = require('cors')({ origin: true });
 
 exports.deleteOrderHandler = ((req, res) => {
-  var id = req.body.id;
+  cors(req, res, async () => {
+    var id = req.body.id;
 
-  var APP_ID = req.body.app_id;
-  if (APP_ID == null) {
-    APP_ID = ''
-    // res.status(400).send({ success: false, message: "APP ID could not be null", error: error });
-    // return
-  }
+    var APP_ID = req.body.app_id;
+    if (APP_ID == null) {
+      APP_ID = ''
+      // res.status(400).send({ success: false, message: "APP ID could not be null", error: error });
+      // return
+    }
 
-  let categoryRef = db.collection(APP_ID + 'Categories').doc(id);
+    let categoryRef = db.collection(APP_ID + 'Categories').doc(id);
 
-  db.collection(APP_ID + 'Products').where('catId', '==', id).get()
-    .then(snapshot => {
-      if (snapshot.empty) {
-        console.log('No Products found for this category');
-        return categoryRef.delete();
-      }
-      let batch = db.batch();
-      batch.delete(categoryRef);
+    db.collection(APP_ID + 'Products').where('catId', '==', id).get()
+      .then(snapshot => {
+        if (snapshot.empty) {
+          console.log('No Products found for this category');
+          return categoryRef.delete();
+        }
+        let batch = db.batch();
+        batch.delete(categoryRef);
 
-      snapshot.forEach(product => {
-        let productRef = db.collection(APP_ID + 'Products').doc(product.id);
-        batch.delete(productRef);
+        snapshot.forEach(product => {
+          let productRef = db.collection(APP_ID + 'Products').doc(product.id);
+          batch.delete(productRef);
+        });
+        return batch.commit();
+      })
+      .then(data => {
+        res.status(200).send({ success: true, messageEng: "Category removed", message: "类别已删除", data: data });
+      })
+      .catch(err => {
+        console.log('Error getting documents', err);
+        res.status(404).send({ success: false, messageEng: "Error while removing Category", message: "删除类别时出错", error: err });
       });
-      return batch.commit();
-    })
-    .then(data => {
-      res.status(200).send({ success: true, messageEng: "Category removed", message: "类别已删除", data: data });
-    })
-    .catch(err => {
-      console.log('Error getting documents', err);
-      res.status(404).send({ success: false, messageEng: "Error while removing Category", message: "删除类别时出错", error: err });
-    });
+  });
 });
--- functions/deleteServiceCategory.js
+++ functions/deleteServiceCategory.js
@@ -1,38 +1,41 @@
 const admin = require('firebase-admin');
 const db = admin.firestore();
+const cors = require('cors')({ origin: true });
 
 exports.deleteServiceCategoryHandler = ((req, res) => {
-  var id = req.body.id;
+  cors(req, res, async () => {
+    var id = req.body.id;
 
-  var APP_ID = req.body.app_id;
-  if (APP_ID == null) {
-    APP_ID = ''
-    // res.status(400).send({ success: false, message: "APP ID could not be null", error: error });
-    // return
-  }
+    var APP_ID = req.body.app_id;
+    if (APP_ID == null) {
+      APP_ID = ''
+      // res.status(400).send({ success: false, message: "APP ID could not be null", error: error });
+      // return
+    }
 
-  let categoryRef = db.collection(APP_ID + 'ServiceCats').doc(id);
+    let categoryRef = db.collection(APP_ID + 'ServiceCats').doc(id);
 
-  db.collection(APP_ID + 'Services').where('catId', '==', id).get()
-    .then(snapshot => {
-      if (snapshot.empty) {
-        console.log('No Service found for this category');
-        return categoryRef.delete();
-      }
-      let batch = db.batch();
-      batch.delete(categoryRef);
+    db.collection(APP_ID + 'Services').where('catId', '==', id).get()
+      .then(snapshot => {
+        if (snapshot.empty) {
+          console.log('No Service found for this category');
+          return categoryRef.delete();
+        }
+        let batch = db.batch();
+        batch.delete(categoryRef);
 
-      snapshot.forEach(product => {
-        let productRef = db.collection(APP_ID + 'Services').doc(product.id);
-        batch.delete(productRef);
+        snapshot.forEach(product => {
+          let productRef = db.collection(APP_ID + 'Services').doc(product.id);
+          batch.delete(productRef);
+        });
+        return batch.commit();
+      })
+      .then(data => {
+        res.status(200).send({ success: true, messageEng: "Service Category removed", message: "服務类别已删除", data: data });
+      })
+      .catch(err => {
+        console.log('Error getting documents', err);
+        res.status(404).send({ success: false, messageEng: "Error while removing Service Category", message: "删除服務类别时出错", error: err });
       });
-      return batch.commit();
-    })
-    .then(data => {
-      res.status(200).send({ success: true, messageEng: "Service Category removed", message: "服務类别已删除", data: data });
-    })
-    .catch(err => {
-      console.log('Error getting documents', err);
-      res.status(404).send({ success: false, messageEng: "Error while removing Service Category", message: "删除服務类别时出错", error: err });
-    });
+  });
 });
